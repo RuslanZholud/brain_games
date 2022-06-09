67a3086d9bda2922ed5c06aa765b07a75de8e02a
Ruslan Zholud <ruslanzholud@gmail.com>
2022-06-09 11:41:31 +0300

    modified brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+from ..cli import welcome_user
+
 def welcome():
     print('Welcome to the Brain Games!')
 
 
 def main():
     welcome()
+    welcome_user()
 
 
 if __name__ == '__main__':
