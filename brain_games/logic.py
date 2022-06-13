ecb4301cd2b18c1e77ee1f21680fbf7835f4dc3c
Ruslan Zholud <ruslanzholud@gmail.com>
2022-06-13 13:30:10 +0300

    fix logic.py

--- brain_games/logic.py
+++ brain_games/logic.py
@@ -28,10 +28,8 @@ def win_message(user_name):
 
 
 def run(user_answer, correct_answer, user_name):
-    attempts = ATTEMPTS
     if correct_answer == user_answer:
         print('Correct!\n')
-        attempts -= 1
     else:
         print(f'''"{user_answer}" is wrong answer ;(. Correct answer was "{correct_answer}".
 Let`s try again {user_name}!\n''')
